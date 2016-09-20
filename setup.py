0236f42ddfdd3b43eb3b8eb945f960cf2d1e15c7
Frederic Hoerni <fhoerni@free.fr>
2016-09-20 21:09:53 +0200

    setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='meseq',
+      version='1.0',
+      description='Editor of Message Sequence Diagrams',
+      author='Fred Hoerni',
+      author_email='fhoerni@free.fr',
+      url='https://github.com/goeb/meseq',
+      py_modules=['meseq']
+     )
